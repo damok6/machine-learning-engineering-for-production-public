86b8ddea47bc31eb70f5c52ee9e30e28c0f4aace
Damian Kelly <damian.t.kelly@gmail.com>
2021-10-09 18:26:46 +0100

    test update

--- course4/week3-ungraded-labs/C4_W3_Lab_4_Github_Actions/app/main.py
+++ course4/week3-ungraded-labs/C4_W3_Lab_4_Github_Actions/app/main.py
@@ -4,7 +4,7 @@ from typing import List
 from fastapi import FastAPI
 from pydantic import BaseModel, conlist
 
-# DK Change
+# DK Change V2
 
 app = FastAPI(title="Predicting Wine Class with batching")
 
